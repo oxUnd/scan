42db5863615ef6a541543b4bebc6d88d079cb766
xiangshouding <fansekey@gmail.com>
2014-07-02 10:57:24 +0800

    update

--- core/worker.go
+++ core/worker.go
@@ -31,42 +31,36 @@ func (w Worker) ScanFtp() (int, error) {
 	return 0, nil
 }
 
-func (w Worker) ScanHttp() (int, error) {
+func (w Worker) ScanHttp() {
 	ports := w.task.GetPorts()
 
 	if len(ports) == 0 {
 		ports = append(ports, 80)
 	}
-	s := make(chan (map[string]error))
+	wait := make(chan (bool))
+	howManyPort := len(ports)
 	for _, port := range ports {
 		go func(port int) {
 			url_ := w.task.GetUrl(port)
-			ret := map[string]error{
-				url_: nil,
-			}
+			ret := false
 			log.Println(url_)
 			res, err := HttpHeadRequest(w.task.GetUrl(port), w.timeout)
 			r := NewReport(rand.Int(), w.task.GetIp(), w.task.GetProtocol(), port)
 			if err != nil {
-				ret[url_] = err
 				r.CatchError(err)
 			} else {
 				r.Add("Header", res.Header)
 				r.Add("Server", res.Header.Get("Server"))
 			}
 			w.addReport(r)
-			s <- ret
+			ret = true
+			wait <- ret
 		}(port)
 	}
 
-	for i := 0; i < len(ports); i++ {
-		r := <-s
-		for k, v := range r {
-			log.Println(k, v)
-		}
+	for i := 0; i < howManyPort; i++ {
+		<-wait
 	}
-
-	return 0, nil
 }
 
 func (w *Worker) addReport(r Report) {
